699c5826dc6e81eafd2c62e244dfb99f031dd0f3
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-06-21 22:28:08 -0700

    added js modal but still need testing

---
+++ public/js/addReciept.js
@@ -0,0 +1,30 @@
+function check_empty() {
+    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+        alert("Fill All Fields !");
+    } else {
+        document.getElementById('form').submit();
+        alert("Form Submitted Successfully...");
+    }
+}
+
+function addRow() {
+    "use strict";
+ 
+     var table = document.getElementById("table");
+     
+     var row= document.createElement("tr");
+     console.log(row);
+     var td1 = document.createElement("td");
+     var td2 = document.createElement("td");
+     var td3 = document.createElement("td");    
+ 
+     td1.innerHTML = document.getElementById("recipient").value;
+     td2.innerHTML  = document.getElementById("item").value;
+     td3.innerHTML  = document.getElementById("price").value;
+ 
+     row.appendChild(td1);
+     row.appendChild(td2);
+     row.appendChild(td3);
+ 
+     table.children[0].appendChild(row);
+};
